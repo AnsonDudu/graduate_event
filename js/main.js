022e52acb28dcd5497814de9cf9769597311bfd0
Anson <ansonchan.dudu@gmail.com>
2017-03-29 18:15:47 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+$(window).load(function() {
+var runPage;
+runPage = new FullPage({
+    id : 'pageContain',                            // id of contain
+    slideTime : 800,                               // time of slide
+    continuous : false,                            // create an infinite feel with no endpoints
+    effect : {                                     // slide effect
+            transform : {
+                translate : 'Y',                   // 'X'|'Y'|'XY'|'none'
+                scale : [.1, 1],                   // [scalefrom, scaleto]
+                rotate : [270, 0]                      // [rotatefrom, rotateto]
+            },
+            opacity : [0, 1]                       // [opacityfrom, opacityto]
+        },
+    mode : 'wheel,touch,nav:navBar',               // mode of fullpage
+    easing : 'ease' ,                              // easing('ease','ease-in','ease-in-out' or use cubic-bezier like [.33, 1.81, 1, 1];
+    callback : function(){
+
+
+             $(".dialogue").removeClass("hide");
+             if(!$(".page4")){
+                 $(".dialogue").addClass("animated tada");
+               }
+             window.setTimeout(function(){
+                $(".dialogue").addClass("hide")
+             },3000)
+    }
+});
+});;
+
+
+//
+
+window.setTimeout(function(){
+    $(".dialogue").removeClass("hide")
+    $(".dialogue").addClass("animated tada");
+    window.setTimeout(function(){
+          $(".dialogue").addClass("hide")
+    },3000)
+}, 1000);
