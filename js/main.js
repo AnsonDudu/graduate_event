947d531a87270ec9906e3c4dbe6c2050875e0fef
Anson <ansonchan.dudu@gmail.com>
2017-03-31 14:20:45 +0800

    first-line2-commit

--- js/main.js
+++ js/main.js
@@ -1,38 +1,6 @@
-$(window).load(function() {
-var runPage;
-runPage = new FullPage({
-    id : 'pageContain',                            // id of contain
-    slideTime : 800,                               // time of slide
-    continuous : false,                            // create an infinite feel with no endpoints
-    effect : {                                     // slide effect
-            transform : {
-                translate : 'Y',                   // 'X'|'Y'|'XY'|'none'
-                scale : [.1, 1],                   // [scalefrom, scaleto]
-                rotate : [270, 0]                      // [rotatefrom, rotateto]
-            },
-            opacity : [0, 1]                       // [opacityfrom, opacityto]
-        },
-    mode : 'wheel,touch,nav:navBar',               // mode of fullpage
-    easing : 'ease' ,                              // easing('ease','ease-in','ease-in-out' or use cubic-bezier like [.33, 1.81, 1, 1];
-    callback :
-    function(){
-        var freashBack =window.setInterval(function(){
-                $(".dialogue").addClass("hide");
-            },2000);
-        $(".dialogue").removeClass("hide");
-        $(".dialogue2").removeClass("hide");
-        freashBack;
-    }
+$("#graduate_event").fullpage({
+	scrollingSpeed: 400,
+	navigation:true,
+	navigationPosition: "left",
 });
-});;
 
-
-//
-
-window.setTimeout(function(){
-    $(".dialogue").removeClass("hide")
-    $(".dialogue").addClass("animated tada");
-    window.setTimeout(function(){
-          $(".dialogue").addClass("hide")
-    },3000)
-}, 1000);
